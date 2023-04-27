bed0f30707b3378cd40a81d0b08e8a19a240a40a
Metodi Mitkov <s8memitk@stud.uni-saarland.de>
2023-04-27 19:22:59 +0200

    Renamed SaveStats and URLFeedback to URLs and URL

--- modules/urls.py
+++ modules/urls.py
@@ -2,35 +2,33 @@ from datetime import datetime
 from logging import Logger
 from typing import List, Optional, Tuple
 
-from peewee import DateTimeField, IntegerField, TextField
+from peewee import DateTimeField, ForeignKeyField, IntegerField, TextField
 from playwright.sync_api import Response
 
 from config import Config
-from database import BaseModel, database
+from database import BaseModel, Task, database
 from modules.module import Module
 
 
-class URLFeedback(BaseModel):
-    job = TextField()
-    crawler = IntegerField()
-    site = TextField()
+class URL(BaseModel):
+    task = ForeignKeyField(Task)
     url = TextField()
     urlfinal = TextField()
+    fromurl = TextField(default=None, null=True)
+    fromurlfinal = TextField(default=None, null=True)
     depth = IntegerField()
     code = IntegerField()
-    fromurl = TextField(null=True)
-    fromurlfinal = TextField(null=True)
+    repetition = IntegerField()
     start = DateTimeField()
     end = DateTimeField()
-    repetition = IntegerField()
 
 
-class SaveStats(Module):
+class URLs(Module):
     @staticmethod
     def register_job(log: Logger) -> None:
         log.info('Create feedback stats table')
         with database:
-            database.create_tables([URLFeedback])
+            database.create_tables([URL])
 
     def receive_response(self, responses: List[Optional[Response]],
                          url: Tuple[str, int, int, List[Tuple[str, str]]], final_url: str,
@@ -42,9 +40,8 @@ class SaveStats(Module):
                 code: int = response.status if response is not None else Config.ERROR_CODES['response_error']
                 fromurl: Optional[str] = url[3][-1][0] if len(url[3]) > 0 else None
                 fromurlfinal: Optional[str] = url[3][-1][1] if len(url[3]) > 0 else None
-                URLFeedback.create(job=self.crawler.job_id, crawler=self.crawler.crawler_id,
-                                   site=self.crawler.site, url=url[0],
-                                   urlfinal=self.crawler.page.url, depth=self.crawler.depth,
-                                   code=code, fromurl=fromurl, fromurlfinal=fromurlfinal,
-                                   start=start[i], end=datetime.now(),
-                                   repetition=self.crawler.repetition)
+
+                URL.create(task=self.crawler.task, url=url[0], urlfinal=self.crawler.page.url,
+                           depth=self.crawler.depth, code=code, fromurl=fromurl,
+                           fromurlfinal=fromurlfinal, start=start[i], end=datetime.now(),
+                           repetition=self.crawler.repetition)
