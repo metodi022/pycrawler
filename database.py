ad61e99ca81b407879532867ef280a0fbc3de0ab
Metodi Mitkov <s8memitk@stud.uni-saarland.de>
2023-12-01 20:40:33 +0100

    Fixed database.py code clarity

--- database.py
+++ database.py
@@ -36,6 +36,7 @@ class Task(BaseModel):
     state = TextField(default='free')
     code = IntegerField(null=True)
     error = TextField(null=True)
+    crawlerState = TextField(null=True)
 
 
 class URL(BaseModel):
@@ -58,87 +59,57 @@ class URLDB:
         self.crawler: Crawler = crawler
         self._seen: MutableSet[str] = set()
 
-    def get_active(self) -> int:
-        return URL.select().where(URL.task == self.crawler.task,
-                                  URL.job == self.crawler.job_id,
-                                  URL.crawler == self.crawler.crawler_id,
-                                  URL.site == self.crawler.site,
-                                  URL.state != 'complete').count()
-
     def get_url(self, repetition: int) -> Optional[URL]:
         url: Optional[URL] = None
 
-        if repetition == 1:
+        query = [
+            URL.task == self.crawler.task,
+            URL.job == self.crawler.job_id,
+            URL.crawler == self.crawler.crawler_id,
+            URL.site == self.crawler.site,
+            URL.repetition == repetition
+        ]
+
+        if (repetition == 1):
             if Config.BREADTHFIRST:
-                url = URL.select().where(
-                    URL.task == self.crawler.task,
-                    URL.job == self.crawler.job_id,
-                    URL.crawler == self.crawler.crawler_id,
-                    URL.site == self.crawler.site,
-                    URL.depth == self.crawler.depth,
-                    URL.repetition == repetition,
-                    URL.state == 'free'
-                ).order_by(URL.created.asc()).first()
+                query.append(URL.depth == self.crawler.depth)
 
-
-            url = url or URL.select().where(
-                URL.task == self.crawler.task,
-                URL.job == self.crawler.job_id,
-                URL.crawler == self.crawler.crawler_id,
-                URL.site == self.crawler.site,
-                URL.repetition == repetition,
-                URL.state == 'free'
-            ).order_by(URL.created.asc()).first()
+            url = URL.select().where(*query, URL.state == "free").order_by(URL.created.asc()).first()
         else:
-            url = URL.get_or_none(task=self.crawler.task,
-                                  job=self.crawler.job_id,
-                                  crawler=self.crawler.crawler_id,
-                                  site=self.crawler.site,
-                                  url=self.crawler.currenturl,
-                                  depth=self.crawler.depth,
-                                  repetition=repetition,
-                                  state='waiting')
+            url = URL.get_or_none(*query, url=self.crawler.currenturl, depth=self.crawler.depth, state="waiting")
 
-        if url is None:
-            return None
-        
-        url.state = 'progress'
-        url.save()
+        if url:
+            url.state = "progress"
+            url.save()
+
         return url
 
     def get_seen(self, url: str) -> bool:
         return url in self._seen
 
     def add_seen(self, url: str):
+        url = url.rstrip('/')
         self._seen.add(url)
-        if url[-1] == '/':
-            self._seen.add(url[:-1])
-        else:
-            self._seen.add(url + '/')
+        self._seen.add(url + '/')
 
     def add_url(self, url: str, depth: int, fromurl: Optional[URL], force: bool = False) -> None:
         if (url in self._seen) and (not force):
             return
-        
+
         self.add_seen(url)
 
+        url_data = {
+            "task": self.crawler.task,
+            "job": self.crawler.job_id,
+            "crawler": self.crawler.crawler_id,
+            "site": self.crawler.site,
+            "url": url,
+            "fromurl": fromurl,
+            "depth": depth
+        }
+
         with database.atomic():
-            URL.create(task=self.crawler.task,
-                       job=self.crawler.job_id,
-                       crawler=self.crawler.crawler_id,
-                       site=self.crawler.site,
-                       url=url,
-                       fromurl=fromurl,
-                       depth=depth,
-                       repetition=1)
-            
+            URL.create(**url_data, repetition=1)
+
             for repetition in range(2, Config.REPETITIONS + 1):
-                URL.create(task=self.crawler.task,
-                           job=self.crawler.job_id,
-                           crawler=self.crawler.crawler_id,
-                           site=self.crawler.site,
-                           url=url,
-                           fromurl=fromurl,
-                           depth=depth,
-                           repetition=repetition,
-                           state='waiting')
+                URL.create(**url_data, repetition=repetition, state='waiting')
